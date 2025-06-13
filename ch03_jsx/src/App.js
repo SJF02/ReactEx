1ccb4b89f6d6b59f826879989eba7a33cb40e35d
SJF02 <suhodo1126@gmail.com>
2025-06-13 14:59:20 +0900

    리액트 기본 기능

---
+++ ch03_jsx/src/App.js
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+// import Child from './ex01_var/Child';
+// import Child from './ex02_op/Child';
+// import Child from './ex03_and/Child';
+// import Child from './ex04_or/Child';
+import Child from './ex05_style/Child';
+
+function App() {
+  return (
+    // <Child>   // 이것은 사용할 수 없다.
+
+    // <Child></Child>     // 사용 가능
+
+    <Child/>
+  );
+}
+
+export default App;
