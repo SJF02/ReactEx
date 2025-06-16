bd1604ac5a53695ed36cbcb8f04e510f96360862
SJF02 <suhodo1126@gmail.com>
2025-06-16 10:54:28 +0900

    Comment 컴포넌트 제작

--- ch04_component/src/ex13_commentlist/Comment.jsx
+++ ch04_component/src/ex13_commentlist/Comment.jsx
@@ -29,3 +29,20 @@ const styles = {
         fontSize: 16,
     }
 };
+
+function Comment(props){
+    return (
+        <div style={styles.wrapper}>
+            <div style={styles.imageContainer}>
+                <img src = "https://cdn-icons-png.flaticon.com/512/2648/2648723.png" style={styles.image} alt=""/>
+            </div>
+
+            <div style={styles.contentContainer}>
+                <span style={styles.nameText}>Albert</span>
+                <span style={styles.commentText}>나의 댓글 컴포넌트입니다.</span>
+            </div>
+        </div>
+    )
+}
+
+export default Comment;
