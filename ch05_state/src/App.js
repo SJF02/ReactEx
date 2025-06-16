70bc27cb0b41cf5ea0807f7bd1a6875413beef94
SJF02 <suhodo1126@gmail.com>
2025-06-16 12:06:31 +0900

    state 훅

---
+++ ch05_state/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// import Counter from './ex01_state/Counter';
+import Say from './ex02_state/Say';
+
+function App() {
+  return (
+    // <Counter/>
+
+    <Say/>
+  );
+}
+
+export default App;
