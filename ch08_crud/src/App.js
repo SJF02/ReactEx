b6cda3bdf2cb1e5179811db7a53ed1841bd89fc1
SJF02 <suhodo1126@gmail.com>
2025-06-17 12:14:49 +0900

    <Nav> 영역 이벤트 전달

--- ch08_crud/src/App.js
+++ ch08_crud/src/App.js
@@ -7,7 +7,8 @@ import './App.css';
 // import SubApp from './ex05_list/SubApp';
 // import SubApp from './ex06_nav_list/SubApp';
 // import SubApp from './ex07_nav_list/SubApp';
-import SubApp from './ex08_header_event/SubApp';
+// import SubApp from './ex08_header_event/SubApp';
+import SubApp from './ex09_nav_event/SubApp';
 
 
 function App() {
