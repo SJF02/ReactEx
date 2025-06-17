6c39e1c2d1d3543f7c1ea32eac8384fdbc384fa7
SJF02 <suhodo1126@gmail.com>
2025-06-17 11:19:40 +0900

    부분 컴포넌트로 분할

--- ch08_crud/src/App.js
+++ ch08_crud/src/App.js
@@ -1,24 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+// import SubApp from './ex01_full_html/SubApp';
+// import SubApp from './ex02_divide/SubApp';
+import SubApp from './ex03_divide/SubApp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SubApp/>
   );
 }
 
