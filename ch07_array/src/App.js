74f25495da0ac94efd7989ec58f6e4a31637e930
SJF02 <suhodo1126@gmail.com>
2025-06-17 10:21:02 +0900

    더블클릭시 삭제하기 코딩중...

--- ch07_array/src/App.js
+++ ch07_array/src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 // import Iteration from './ex01_map/Iteration';
 // import Iteration from './ex02_key/Iteration';
-import Iteration from './ex03_id/Iteration';
+// import Iteration from './ex03_id/Iteration';
+// import Iteration from './ex04_add_name/Iteration';
+import Iteration from './ex05_remove_name/Iteration';
 
 function App() {
   return (
