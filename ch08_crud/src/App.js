d97d87b7d601dbbc0741b5cac5cb07636a00a89a
SJF02 <suhodo1126@gmail.com>
2025-06-18 09:46:23 +0900

    mode 변경

--- ch08_crud/src/App.js
+++ ch08_crud/src/App.js
@@ -8,7 +8,8 @@ import './App.css';
 // import SubApp from './ex06_nav_list/SubApp';
 // import SubApp from './ex07_nav_list/SubApp';
 // import SubApp from './ex08_header_event/SubApp';
-import SubApp from './ex09_nav_event/SubApp';
+// import SubApp from './ex09_nav_event/SubApp';
+import SubApp from './ex10_state/SubApp';
 
 
 function App() {
