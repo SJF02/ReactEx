7505faab823d3896f4ae2d7fdcf0653782305a4a
SJF02 <suhodo1126@gmail.com>
2025-06-19 16:00:08 +0900

    useEffect 구현

--- ch10_hooks/src/App.js
+++ ch10_hooks/src/App.js
@@ -1,7 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 // import Counter from './ch01_useState/Counter';
-import Info from './ch02_useState/Info';
+// import Info from './ch02_useState/Info';
+// import Info from './ch03_useEffect/Info';
+// import Info from './ch04_useEffect/Info';
+// import Info from './ch05_useEffect/Info';
+import Info from './ch06_useEffect/Info';
 
 function App() {
   return (
