62c2155761773a1eafb6f0fdd57e276ce38de4ed
SJF02 <suhodo1126@gmail.com>
2025-06-19 13:20:49 +0900

    클래스 컴포넌트의 라이프 사이클

--- ch09_lifecycle/src/components/NotificationList.jsx
+++ ch09_lifecycle/src/components/NotificationList.jsx
@@ -16,17 +16,23 @@ const reservedNotifications = [
     },
 ];
 
+// 클래스 컴포넌트는 반드시 Component를 상속받아야 한다.
 class NotificationList extends Component{
+    // 생성자
     constructor(props){
         super(props);
 
         this.timer = null;
 
+        // 함수 컴포넌트의 useState와 동일함.
         this.state = {
             notifications: [],
         };
+
+        console.log("NotificationList 생성자");
     }
 
+    // Mouse가 완료되는 시점에 호출(컴포넌트가 dom에 연결), 주로 동작 초기화시 많이 사용한다.
     componentDidMount(){
         const { notifications } = this.state;
 
@@ -45,13 +51,19 @@ class NotificationList extends Component{
                 this.timer = null;
             }
         }, 1000);
+
+        console.log("NotificationList-componentDidMount()");
     }
 
+    // Unmount될 때 호출(종료 시점)
     componentWillUnmount(){
         if(this.timer) clearInterval(this.timer);
+        console.log("NotificationList-componentWillUnmount()");
     }
 
+    // 화면을 그려야 할 때(시작하고 나서, state가 변경될 때)
     render(){
+        console.log("NotificationList-render()");
         return (
             <div>
                 {this.state.notifications.map((notification)=>{
