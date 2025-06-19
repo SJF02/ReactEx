34d8db633b3d552606ab7b55d7446b9136ea7f51
SJF02 <suhodo1126@gmail.com>
2025-06-19 15:41:39 +0900

    useState 사용법

---
+++ ch10_hooks/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+// import Counter from './ch01_useState/Counter';
+import Info from './ch02_useState/Info';
+
+function App() {
+  return (
+    // <Counter />
+
+    <Info />
+  );
+}
+
+export default App;
