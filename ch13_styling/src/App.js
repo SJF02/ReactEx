90d7fd63d0d6a1620b432c7a139e79ad03ee0a90
SJF02 <suhodo1126@gmail.com>
2025-06-20 15:17:10 +0900

    App.js에 CSSModule 적용

--- ch13_styling/src/App.js
+++ ch13_styling/src/App.js
@@ -1,11 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 // import SassComponent from './ex01_sass/SassComponent';
-import SassComponent from './ex02_sass/SassComponent';
+// import SassComponent from './ex02_sass/SassComponent';
+import CSSModule from './ex03_css_module/CSSModule';
 
 function App() {
   return (
-    <SassComponent />
+    // <SassComponent />
+
+    <CSSModule />
   );
 }
 
