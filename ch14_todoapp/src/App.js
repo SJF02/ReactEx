e5a2bcbd6dd8de50df543676b4d6fb78a6c6ca70
SJF02 <suhodo1126@gmail.com>
2025-06-20 15:56:35 +0900

    TodoTemplate.scss 작성, 적용

--- ch14_todoapp/src/App.js
+++ ch14_todoapp/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import TodoApp from './ex01_TodoTemplate/TodoApp';
+// import TodoApp from './ex01_TodoTemplate/TodoApp';
+import TodoApp from './ex02_TodoTemplate/TodoApp';
 
 function App() {
   return (
