82fce81f0ab66b24e620061b0ee4a53b1ebc8d0c
SJF02 <suhodo1126@gmail.com>
2025-06-20 16:40:18 +0900

    TodoListItem.jsx와 TodoListItem.scss 제작

--- ch14_todoapp/src/App.js
+++ ch14_todoapp/src/App.js
@@ -3,7 +3,8 @@ import './App.css';
 // import TodoApp from './ex01_TodoTemplate/TodoApp';
 // import TodoApp from './ex02_TodoTemplate/TodoApp';
 // import TodoApp from './ex03_TodoInsert/TodoApp';
-import TodoApp from './ex04_TodoList/TodoApp';
+// import TodoApp from './ex04_TodoList/TodoApp';
+import TodoApp from './ex05_TodoListItem/TodoApp';
 
 function App() {
   return (
