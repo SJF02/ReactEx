c3fa49d5924b5128b02065cb9535496cc57e3d4f
SJF02 <suhodo1126@gmail.com>
2025-06-20 16:56:30 +0900

    TodoList와 TodoListItem에 배열전달, checkbox기능 추가

---
+++ ch14_todoapp/src/ex06_TodoList/TodoListItem.jsx
@@ -0,0 +1,26 @@
+import {
+  MdCheckBoxOutlineBlank,
+  MdRemoveCircleOutline,
+  MdCheckBox,
+} from "react-icons/md";
+import "./TodoListItem.scss";
+import cn from "classnames";
+
+const TodoListItem = (props) => {
+  const { todo } = props;
+  const { text, checked } = todo;
+
+  return (
+    <div className="TodoListItem">
+      <div className={cn("checkbox", { checked })}>
+        {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
+        <div className="text">{text}</div>
+      </div>
+      <div className="remove">
+        <MdRemoveCircleOutline />
+      </div>
+    </div>
+  );
+};
+
+export default TodoListItem;
