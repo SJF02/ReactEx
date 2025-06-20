5a8289c7c3dec6f14f5c1f9848de13000b27f5cd
SJF02 <suhodo1126@gmail.com>
2025-06-20 17:07:15 +0900

    주석 추가

--- ch14_todoapp/src/ex06_TodoList/TodoListItem.jsx
+++ ch14_todoapp/src/ex06_TodoList/TodoListItem.jsx
@@ -12,6 +12,7 @@ const TodoListItem = (props) => {
 
   return (
     <div className="TodoListItem">
+      {/* checked가 true면 checkbox클래스가 적용되고, false면 checkbox가 적용되지 않음 */}
       <div className={cn("checkbox", { checked })}>
         {checked ? <MdCheckBox /> : <MdCheckBoxOutlineBlank />}
         <div className="text">{text}</div>
