c1b3417fd1541f58217061741dcce2462ce050e3
SJF02 <suhodo1126@gmail.com>
2025-06-20 15:00:31 +0900

    scss 문법

--- ch13_styling/src/App.js
+++ ch13_styling/src/App.js
@@ -1,24 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+// import SassComponent from './ex01_sass/SassComponent';
+import SassComponent from './ex02_sass/SassComponent';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SassComponent />
   );
 }
 
