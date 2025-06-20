ecb708526bcc8da6553f73e01667ef4d5f2f513c
SJF02 <suhodo1126@gmail.com>
2025-06-20 13:20:58 +0900

    스타일드 컴포넌트 진행중

--- ch12_styled/src/App.js
+++ ch12_styled/src/App.js
@@ -1,12 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 // import ReactButton from './ex01_nonstyled/ReactButton';
-import StyledButton from './ex02_styled/StyledButton';
+// import StyledButton from './ex02_styled/StyledButton';
+// import SimpleButton from './ex03_styled/SimpleButton';
+// import SimpleButton from './ex04_styled/SimpleButton';
+// import LargeButton from './ex05_styled/LargeButton';
+// import PrimaryButton from './ex06_styled/PrimaryButton';
+// import PrimaryButton from './ex07_styled/PrimaryButton';
+// import PrimaryButton from './ex08_styled/PrimaryButton';
+import PrimaryButton from "./ex09_styled/PrimaryButton";
+import SecondButton from "./ex09_styled/SecondButton";
 
 function App() {
   return (
     // <ReactButton>안녕하세요</ReactButton>
-    <StyledButton>감사합니다</StyledButton>
+    // <StyledButton>감사합니다</StyledButton>
+    // <SimpleButton>존경합니다</SimpleButton>
+    // <LargeButton>사랑합니다</LargeButton>
+
+    // <>
+    //   <PrimaryButton primary>감사합니다</PrimaryButton>
+    //   <PrimaryButton>감사합니다</PrimaryButton>
+    // </>
+
+    <>
+      <PrimaryButton primary>감사합니다</PrimaryButton>
+      <PrimaryButton>감사합니다</PrimaryButton>
+      <SecondButton primary>Happy ^^</SecondButton>
+      <SecondButton>Good~</SecondButton>
+    </>
   );
 }
 
--- ch12_styled/src/ex02_styled/StyledButton.jsx
+++ ch12_styled/src/ex02_styled/StyledButton.jsx
@@ -4,9 +4,9 @@ import styled from "styled-components";
 
 // js 스타일 대신 css 스타일을 그래도 사용할 수 있다.
 const StyledButton = styled.button`
-    color: white,
-    background-color: purple,
-    margin: 10px
+    color: white;
+    background-color: purple;
+    margin: 10px;
 `;
 
 export default StyledButton;
