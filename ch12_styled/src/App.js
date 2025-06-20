fe6b8b514d63cbf5615617f5960fa8e0f6f8a3a3
SJF02 <suhodo1126@gmail.com>
2025-06-20 12:52:28 +0900

    스타일드 컴포넌트 사용

---
+++ ch12_styled/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+// import ReactButton from './ex01_nonstyled/ReactButton';
+import StyledButton from './ex02_styled/StyledButton';
+
+function App() {
+  return (
+    // <ReactButton>안녕하세요</ReactButton>
+    <StyledButton>감사합니다</StyledButton>
+  );
+}
+
+export default App;
