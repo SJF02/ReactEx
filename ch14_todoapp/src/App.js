0c6c6486571cefe2c7a65850b90360a51dad6308
SJF02 <suhodo1126@gmail.com>
2025-06-20 17:22:46 +0900

    TodoInsert에서 입력데이터를 저장

--- ch14_todoapp/src/App.js
+++ ch14_todoapp/src/App.js
@@ -5,7 +5,8 @@ import './App.css';
 // import TodoApp from './ex03_TodoInsert/TodoApp';
 // import TodoApp from './ex04_TodoList/TodoApp';
 // import TodoApp from './ex05_TodoListItem/TodoApp';
-import TodoApp from './ex06_TodoList/TodoApp';
+// import TodoApp from './ex06_TodoList/TodoApp';
+import TodoApp from './ex07_TodoInsert/TodoApp';
 
 function App() {
   return (
