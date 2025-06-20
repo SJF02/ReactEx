88ad2507497a54af86ae034378f5e63b7256d3a7
SJF02 <suhodo1126@gmail.com>
2025-06-20 16:00:18 +0900

    TodoInsert 추가 예정

--- ch14_todoapp/src/App.js
+++ ch14_todoapp/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 // import TodoApp from './ex01_TodoTemplate/TodoApp';
-import TodoApp from './ex02_TodoTemplate/TodoApp';
+// import TodoApp from './ex02_TodoTemplate/TodoApp';
+import TodoApp from './ex03_TodoInsert/TodoApp';
 
 function App() {
   return (
