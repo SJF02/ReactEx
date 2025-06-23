f9e7aab2960cb52719d798fab2ccb57027751f57
SJF02 <suhodo1126@gmail.com>
2025-06-23 14:20:58 +0900

    Card의 확장 사용

--- ch16_composition_inheritance/src/ex04_inheritance/CookCard.jsx
+++ ch16_composition_inheritance/src/ex04_inheritance/CookCard.jsx
@@ -5,6 +5,7 @@ const CookCard = (props)=>{
         <div>
             <Card title="Alex" backgroundColor="#a0434e">
                 <p>알렉스의 요리</p>
+                <img style={{width:"300px", height:"250px"}} src="https://static.wtable.co.kr/image/production/service/recipe/1067/46f0a939-b3e2-4c1e-a2a2-0512f46fab25.jpg" alt="로제 떡볶이" />
                 <ul>
                     <li>두부김치 카나페</li>
                     <li>로제 떡볶이</li>
