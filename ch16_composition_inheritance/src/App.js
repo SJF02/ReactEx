b1a45c79601a267d7351a8c83010ebaa53df3e67
SJF02 <suhodo1126@gmail.com>
2025-06-23 13:03:07 +0900

    App에 SplitPane 적용

--- ch16_composition_inheritance/src/App.js
+++ ch16_composition_inheritance/src/App.js
@@ -1,14 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
-import WelcomeDesc from './ex01_containment/WelcomeDesc';
-import WelcomeDialog from './ex01_containment/WelcomeDialog';
+// import WelcomeDesc from './ex01_containment/WelcomeDesc';
+// import WelcomeDialog from './ex01_containment/WelcomeDialog';
+
+import SplitPane from './ex02_containment/SplitPane';
+import WelcomeDesc from './ex02_containment/WelcomeDesc';
+import WelcomeDialog from './ex02_containment/WelcomeDialog';
 
 function App() {
   return (
-    <div>
-      <WelcomeDesc />
-      <WelcomeDialog />
-    </div>
+
+    // <div>
+    //   <WelcomeDesc />
+    //   <WelcomeDialog />
+    // </div>
+
+    <SplitPane npx={7} color="gray"
+              left={<WelcomeDesc npx={3} color="red" />}
+              right={<WelcomeDialog npx={3} color="blue" />} />
+
   );
 }
 
