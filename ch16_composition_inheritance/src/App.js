5ec1989dfccacccabc728d652d12e9399f282d4e
SJF02 <suhodo1126@gmail.com>
2025-06-23 12:54:20 +0900

    FancyBorder의 포함 용법

---
+++ ch16_composition_inheritance/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import WelcomeDesc from './ex01_containment/WelcomeDesc';
+import WelcomeDialog from './ex01_containment/WelcomeDialog';
+
+function App() {
+  return (
+    <div>
+      <WelcomeDesc />
+      <WelcomeDialog />
+    </div>
+  );
+}
+
+export default App;
