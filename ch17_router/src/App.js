f16bd4119831c94c50e6b7699dc8246ba957e608
SJF02 <suhodo1126@gmail.com>
2025-06-23 15:38:36 +0900

    Nested Routing 구현

--- ch17_router/src/App.js
+++ ch17_router/src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 // import SubApp from './ex01_components/SubApp';
 // import SubApp from './ex02_route/SubApp';
-import SubApp from './ex03_link/SubApp';
+// import SubApp from './ex03_link/SubApp';
+// import SubApp from './ex04_nav_link/SubApp';
+import SubApp from './ex05_nested_routing/SubApp';
 
 function App() {
   return (
