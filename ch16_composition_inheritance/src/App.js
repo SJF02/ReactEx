2cb532fea39fb3998c412563fb3ccaf1c982f0da
SJF02 <suhodo1126@gmail.com>
2025-06-23 13:14:28 +0900

    Specialization의 예시

--- ch16_composition_inheritance/src/App.js
+++ ch16_composition_inheritance/src/App.js
@@ -3,9 +3,12 @@ import './App.css';
 // import WelcomeDesc from './ex01_containment/WelcomeDesc';
 // import WelcomeDialog from './ex01_containment/WelcomeDialog';
 
-import SplitPane from './ex02_containment/SplitPane';
-import WelcomeDesc from './ex02_containment/WelcomeDesc';
-import WelcomeDialog from './ex02_containment/WelcomeDialog';
+// import SplitPane from './ex02_containment/SplitPane';
+// import WelcomeDesc from './ex02_containment/WelcomeDesc';
+// import WelcomeDialog from './ex02_containment/WelcomeDialog';
+
+import AlramDialog from './ex03_specialization/AlramDialog';
+import WelcomeDialog from './ex03_specialization/WelcomeDialog';
 
 function App() {
   return (
@@ -15,9 +18,14 @@ function App() {
     //   <WelcomeDialog />
     // </div>
 
-    <SplitPane npx={7} color="gray"
-              left={<WelcomeDesc npx={3} color="red" />}
-              right={<WelcomeDialog npx={3} color="blue" />} />
+    // <SplitPane npx={7} color="gray"
+    //           left={<WelcomeDesc npx={3} color="red" />}
+    //           right={<WelcomeDialog npx={3} color="blue" />} />
+
+    <div>
+      <AlramDialog />
+      <WelcomeDialog />
+    </div>
 
   );
 }
