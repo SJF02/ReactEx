566234a160c5cacd4eb854a2319d3f8e3b657834
SJF02 <suhodo1126@gmail.com>
2025-06-23 12:08:03 +0900

    부모 상태, 상태변화함수 자식한테 공유

--- ch15_shared_state/src/App.js
+++ ch15_shared_state/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 // import Calculator from './ex01_component/Calculator';
-import Calculator from './ex02_temp_input/Calculator';
+// import Calculator from './ex02_temp_input/Calculator';
+import Calculator from './ex03_shared_state/Calculator';
 
 function App() {
   return (
