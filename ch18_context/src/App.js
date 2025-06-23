c112c5c147bdb72959c4829c30870beb4f00e714
SJF02 <suhodo1126@gmail.com>
2025-06-23 16:44:07 +0900

    Context 영역에 2개 객체 저장

--- ch18_context/src/App.js
+++ ch18_context/src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 // import MainApp from './ex01_hierachy/MainApp';
-import MainApp from './ex02_divide/MainApp';
+// import MainApp from './ex02_divide/MainApp';
+import MainApp from './ex03_context/MainApp';
 
 function App() {
   return (
