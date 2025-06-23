ce744cfb6783ef6955255f471b18639197d18c88
SJF02 <suhodo1126@gmail.com>
2025-06-23 14:47:02 +0900

    Link에 to 변경

--- ch17_router/src/ex03_link/SubApp.jsx
+++ ch17_router/src/ex03_link/SubApp.jsx
@@ -11,9 +11,9 @@ const SubApp = () => {
       <div>
         <h1>Hello React Router DOM</h1>
         <ul>
-            <li><Link href="/">Home</Link></li>
-            <li><Link href="/topics">Topics</Link></li>
-            <li><Link href="/contact">Contact</Link></li>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/topics">Topics</Link></li>
+            <li><Link to="/contact">Contact</Link></li>
         </ul>
         <Routes>
             <Route path="/" element={<Home/>} />
